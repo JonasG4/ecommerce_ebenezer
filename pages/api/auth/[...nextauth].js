8c6b652d7c50f44b7d8e725faf6d2873f83648c0
Jonas Garcia <48955198+JonasG4@users.noreply.github.com>
2023-07-13 11:42:20 -0600

    corrigiendo errores

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -141,12 +141,10 @@ export const authOptions = {
       return session;
     }
   },
-  secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
-  debug: process.env.NODE_ENV === "development",
   pages: {
     signIn: "/nx-admin/auth/login",
   },
